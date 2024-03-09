cc96931c91ecfc06044232fbcc3c4230b8074dd1
Fabián Torres <nftorresc@gmail.com>
2024-03-09 15:22:06 -0500

    Refactorización

--- js/main.js
+++ js/main.js
@@ -3,49 +3,63 @@ const botones = document.querySelectorAll(".btn");
 
 botones.forEach((boton) => {
     boton.addEventListener("click", () => {
-        const textoPantalla = pantalla.textContent;
-        const botonApretado = boton.textContent;
+        let estado = {
+            textoPantalla: pantalla.textContent,
+            botonApretado: boton.textContent,
+        };
 
-        switch (boton.className) {
-            case "btn control":
-                if (boton.id === "c") {
-                    pantalla.textContent = "0";
-                }
-
-                if (boton.id === "borrar" && textoPantalla.length > 1) {
-                    pantalla.textContent = textoPantalla.slice(0, -1);
-                } else {
-                    pantalla.textContent = "0";
-                }
+        switch (boton.dataset.type) {
+            case "control":
+                manejarBotonesControl(boton, estado);
                 break;
-            case "btn operador":
-                let ultimoCaracter = textoPantalla.slice(-1);
-
-                if (ultimoCaracter !== botonApretado) {
-                    pantalla.textContent += botonApretado;
-                }
-
-                let operadores = ["/", "*", "-", "+"];
-
-                if (operadores.includes(ultimoCaracter)) {
-                    pantalla.textContent = textoPantalla.replace(
-                        ultimoCaracter,
-                        botonApretado
-                    );
-                }
-
-                if (boton.id === "igual") {
-                    let resultado = eval(textoPantalla);
-                    pantalla.textContent = resultado;
-                }
+            case "operador":
+                manejarBotonesOperador(boton, estado);
                 break;
-            case "btn numero":
-                if (pantalla.textContent === "0") {
-                    pantalla.textContent = botonApretado;
-                } else {
-                    pantalla.textContent += botonApretado;
-                }
+            case "numero":
+                manejarBotonesNumero(estado);
                 break;
         }
     });
 });
+
+function manejarBotonesControl(boton, estado) {
+    if (boton.id === "c") {
+        pantalla.textContent = "0";
+    }
+
+    if (boton.id === "borrar" && estado.textoPantalla.length > 1) {
+        pantalla.textContent = estado.textoPantalla.slice(0, -1);
+    } else {
+        pantalla.textContent = "0";
+    }
+}
+
+function manejarBotonesOperador(boton, estado) {
+    let ultimoCaracter = estado.textoPantalla.slice(-1);
+
+    if (ultimoCaracter !== estado.botonApretado) {
+        pantalla.textContent += estado.botonApretado;
+    }
+
+    let operadores = ["/", "*", "-", "+"];
+
+    if (operadores.includes(ultimoCaracter)) {
+        pantalla.textContent = estado.textoPantalla.replace(
+            ultimoCaracter,
+            estado.botonApretado
+        );
+    }
+
+    if (boton.id === "igual") {
+        let resultado = eval(estado.textoPantalla);
+        pantalla.textContent = resultado;
+    }
+}
+
+function manejarBotonesNumero(estado) {
+    if (pantalla.textContent === "0") {
+        pantalla.textContent = estado.botonApretado;
+    } else {
+        pantalla.textContent += estado.botonApretado;
+    }
+}
